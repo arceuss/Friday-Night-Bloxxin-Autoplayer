55647852b7c8aa787fabf6883a463fbaff1f1892
o5u3 <69703353+o5u3@users.noreply.github.com>
2022-04-28 15:21:35 -1000

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,5 +1,9 @@
-if is_synapse_function or getexecutorname then
+-- PATCHED, DO NOT EDIT --
+
+--[[if is_synapse_function or getexecutorname then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/o5u3/Friday-Night-Bloxxin-Autoplayer/main/Paid"))()
 else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/o5u3/Friday-Night-Bloxxin-Autoplayer/main/Other"))()
 end
+]]
+loadstring(game:HttpGet("https://raw.githubusercontent.com/o5u3/Friday-Night-Bloxxin-Autoplayer/main/Patched"))()
